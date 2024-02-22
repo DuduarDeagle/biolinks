576857ca508a4775b2eee96264b5f828ed7a0539
Duduar Deagle <imduduar@proton.me>
2024-02-22 09:17:57 -0600

    ⚡️ Add SSR

--- astro.config.js
+++ astro.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config"
 
+import node from "@astrojs/node"
 import react from "@astrojs/react"
 
 export default defineConfig({
+	output: "hybrid",
+	adapter: node({
+		mode: "standalone"
+	}),
 	integrations: [react()],
 	build: {
 		assets: "assets"
