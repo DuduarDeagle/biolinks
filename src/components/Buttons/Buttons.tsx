a515180617d6d01d8f5c1ec9496e21175480e1c5
Duduar Deagle <imduduar@proton.me>
2024-02-22 09:05:04 -0600

    ✨ Add page design

---
+++ src/components/Buttons/Buttons.tsx
@@ -0,0 +1,41 @@
+/*Import dependencies */
+import * as React from "react"
+
+/* Import styles */
+import "./Buttons.css"
+
+/* Define interfaces */
+interface BaseProps {
+	as?: "button" | "a"
+}
+
+interface ButtonProps
+	extends React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {}
+
+interface AnchorProps
+	extends React.DetailedHTMLProps<
+		React.AnchorHTMLAttributes<HTMLAnchorElement>,
+		HTMLAnchorElement
+	> {}
+
+/* Define conditional type */
+type Conditional = BaseProps & (ButtonProps | AnchorProps)
+
+export const Button: React.FC<Conditional> = ({ as = "button", ...props }) => {
+	if (as === "a") {
+		const { href, children } = props as AnchorProps
+
+		return (
+			<a href={href} className="btn">
+				<span className="btn__container">{children}</span>
+			</a>
+		)
+	}
+
+	const { children } = props as ButtonProps
+
+	return <button>{children}</button>
+}
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,54 @@
+/* Import dependencies */
+import * as React from "react"
+
+/* Import styles */
+import "./Footer.css"
+
+/* Define interfaces */
+interface Props
+	extends React.DetailedHTMLProps<
+		React.HTMLAttributes<HTMLElement>,
+		HTMLElement
+	> {}
+
+/* Set up a Footer component */
+const Footer: React.FC<Props> = () => {
+	const date = new Date()
+
+	return (
+		<footer className="footer">
+			<div className={`container footer__container`}>
+				<div className="footer__section-stack">
+					<a target="_blank" href="https://astro.build">
+						<img
+							src="https://astro.badg.es/v2/built-with-astro/tiny.svg"
+							width={120}
+							height={20}
+							alt="Insignia con la frase en inglés 'Built with Astro', que hace referencia al framework con el que fue desarrollada la página."
+						/>
+					</a>
+					<p>
+						<a
+							target="_blank"
+							href="https://github.com/duduardeagle/biolink.git">
+							Código fuente
+						</a>{" "}
+						bajo{" "}
+						<a
+							rel="license"
+							target="_blank"
+							href="https://www.gnu.org/licenses/agpl-3.0.txt">
+							GNU AGPLv3
+						</a>
+					</p>
+				</div>
+				<p className="footer__copyright-notice">
+					Copyright © {date.getFullYear()} Duduar Deagle.{" "}
+					<span>Todos los derechos reservados.</span>
+				</p>
+			</div>
+		</footer>
+	)
+}
+
+export default Footer
